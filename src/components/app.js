e54fbb8a08f70ceb49197565f91ab892c19a1282
Mantas Ivaskevicius <mantasivaskevicius@gmail.com>
2020-02-10 09:12:28 -0500

    w

--- src/components/app.js
+++ src/components/app.js
@@ -31,11 +31,14 @@ class App extends React.Component {
   }
 
   handleCardClick = e => {
-    this.setState({
-      currentProductId: e
-    }, () => {
-      this.updateProduct(e)
-    });
+    this.setState(
+      {
+        currentProductId: e
+      },
+      () => {
+        this.updateProduct(e);
+      }
+    );
   };
 
   updateProduct = id => {
@@ -50,7 +53,6 @@ class App extends React.Component {
     this.updateProduct(2);
     this.updateProduct(this.state.currentProductId);
     this.props.fetchAllProductData(1);
-
   }
 
   render() {
@@ -67,10 +69,13 @@ class App extends React.Component {
             handleCardClick={this.handleCardClick}
           />
 
-          {Object.entries(this.props.currentProduct).length === 0 ? (
+          {Object.entries(this.props.overallData.currentProduct).length ===
+          0 ? (
             <div>LOADING</div>
           ) : (
-            <RatingsAndReviews productData={this.props.currentProduct} />
+            <RatingsAndReviews
+              productData={this.props.overallData.currentProduct}
+            />
           )}
         </Router>
       </>
