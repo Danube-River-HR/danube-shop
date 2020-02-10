250c395a44fac192bdbabe10fa04298307760735
John Lee <johnjun.lee91@gmail.com>
2020-02-10 18:21:09 -0500

    Started developing product gallery

---
+++ src/components/Overview/ProductGallery/ImageThumbnail.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Img from 'react-image';
+
+class ImageThumbnail extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    handleThumbnailClick = () => {
+        let thumbnailData = {
+            mainURL: this.props.mainURL,
+            index: this.props.index
+        }
+        this.props.changeCurrentImage(thumbnailData)
+    }
+
+    renderSelectedHighlighting = () => {
+        if (this.props.selectedThumbnailIndex === this.props.index) {
+            return {
+                border: "2px solid red"
+            }
+        } else {
+            return {}
+        }
+    }
+
+    render() {
+        return (
+            
+                <Img 
+                    src={this.props.thumbnailURL}
+                    onClick={this.handleThumbnailClick}
+                    className="thumbnail-child"
+                    style={this.renderSelectedHighlighting()}
+                    />
+            
+        )
+    }
+}
+
+export default ImageThumbnail;
--- src/components/Overview/ProductGallery/ProductGallery.js
+++ src/components/Overview/ProductGallery/ProductGallery.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
-// import {fetchAllProductData} from "../../../redux/actions/index";
+import MainImage from './MainImage';
+import GalleryThumbnails from './GalleryThumbnails';
 
 class ProductGallery extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-           
+           currentImage: '',
+           thumbnails: [],
+           selectedThumbnailIndex: 0
         }
     }
 
-    componentDidMount() {
-        // fetchAllProductData(1);
+    changeCurrentImage = (thumbnailData) => {
+        this.setState({
+            currentImage: thumbnailData.mainURL,
+            selectedThumbnailIndex: thumbnailData.index
+        })
+    }
+
+    componentDidUpdate(prevProps) {
+        // If our prop loads and currentImage is empty, set a new url there.
+        if (this.props.selectedStyle !== null && this.state.currentImage === '') {
+            this.setState({
+                currentImage: this.props.selectedStyle.photos[0].url,
+                thumbnails: this.props.selectedStyle.photos
+            })
+        } else if (this.props.selectedStyle !== null && this.state.currentImage !== '') {
+            if (this.props.selectedStyle.style_id !== prevProps.selectedStyle.style_id) {
+                this.setState({
+                    currentImage: this.props.selectedStyle.photos[0].url,
+                    thumbnails: this.props.selectedStyle.photos,
+                    selectedThumbnailIndex: 0
+                })
+            }
+        }
+       
     }
 
     render() {
+        console.log('PRODUCT GALLERY PROPS:', this.props);
+
         return (
             <div className="gallery-container">
-                PRODUCT GALLERY FASHO
+                <GalleryThumbnails 
+                    thumbnails={this.state.thumbnails}
+                    changeCurrentImage={this.changeCurrentImage}
+                    selectedThumbnailIndex={this.state.selectedThumbnailIndex}
+                    />
+                <MainImage imageURL={this.state.currentImage}/>
             </div>
         )
     }
@@ -27,11 +59,12 @@ class ProductGallery extends Component {
 
 const mapStateToProps = (state) => {
     // console.log('PRODUCT GALLERY STATE:', state);
+
     return {
-        // currentProduct: state.currentProduct,
-        // productStyles: state.productStyles,
-        // overallData: state.overallData
+        overallData: state.overallData,
+        selectedStyle: state.selectedStyle
       };
 }
+
 
 export default connect(mapStateToProps)(ProductGallery);
