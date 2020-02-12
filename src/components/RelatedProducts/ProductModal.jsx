5cb59f13f8dd450f764096ac424ccd9e0e350a61
Gabriel Ng <kingwai715@gmail.com>
2020-02-12 15:21:50 -0500

    added css styling for comparison modal

--- src/components/RelatedProducts/ProductModal.jsx
+++ src/components/RelatedProducts/ProductModal.jsx
@@ -3,27 +3,48 @@ import { Button, Header, Image, Modal } from "semantic-ui-react";
 import "./ProductModal.css";
 
 const ModalModalExample = props => {
+  // console.log(props.currentProduct,'current product')
   let features = {};
+  console.log(props.currentProduct.features, props.cardData.features, 'compare')
   props.currentProduct.features.forEach(
-    feature => (features[feature.feature] = { current: feature.value })
+    // feature => (features[feature.feature] = { current: feature.value })
+    feature => {
+      features[feature.feature] = {}
+      features[feature.feature]['current'] = feature.value
+      features[feature.feature]['related'] = features[feature.feature]['related'] ? feature.value : '-----'
+    }
+
   );
   props.cardData.features.forEach(
-    feature => (features[feature.feature] = { related: feature.value })
+    feature => {
+      if (!features[feature.feature]) {
+        features[feature.feature] = {}
+      }
+      features[feature.feature]['related'] = feature.value
+      features[feature.feature]['current'] = features[feature.feature]['current'] ? features[feature.feature]['current'] : '-----'
+    }
   );
+  
   // console.log(features,'features obj')
   let productModal = () => {
     let table = [];
     for (let feature in features) {
-      // console.log(features[feature].current,'current')
-       table.push(
-        <div class="modalContainer">
-          <div>{features[feature].current}</div>
-          <div class="feature">{feature}</div>
-          <div>{features[feature].related}</div>
+      // console.log(features,'feature')
+      table.push(
+        // <div class="modalContainer">
+        //   <div>{features[feature].current}</div>
+        //   <div class="feature">{feature}</div>
+        //   <div>{features[feature].related}</div>
+        // </div>
+        <div class="divTableBody">
+          <div class="divTableRow">
+            <div class="divTableCell">{features[feature].current}</div>
+            <div class="divTableCell" style={{fontWeight: 'bold', textTransform: 'uppercase'}}>{feature}</div>
+            <div class="divTableCell">{features[feature].related}</div>
+          </div>
         </div>
       );
     }
-    // console.log(table)
     return table;
   };
 
@@ -38,7 +59,7 @@ const ModalModalExample = props => {
       <Modal.Header>Comparing</Modal.Header>
       <Modal.Content image>
         <Modal.Description>
-          {/* <Header>Comparisons</Header> */}
+          {/* <Header>Comparisons</Header>
           <div class="modalContainer">
           <div>Current Product</div>
           <div>Features</div>
@@ -47,16 +68,17 @@ const ModalModalExample = props => {
           
           <ul>
             {productModal()}
-            {/* {Object.values(features).map(feature => <li>{feature.current}</li>)}
-          </ul>
-          <h5>Features</h5>
-          <ul>
-  {Object.keys(features).map(feature => <li>{feature}</li>)}
-          </ul>
-          <h5>Related Product</h5>
-          <ul>
-  {Object.values(features).map(feature => <li>{feature.related}</li>)} */}
-          </ul>
+          </ul> */}
+          <div class="divTable blueTable">
+            <div class="divTableHeading">
+              <div class="divTableRow">
+                <div class="divTableHead">{props.currentProduct.name}</div>
+                <div class="divTableHead"></div>
+                <div class="divTableHead">{props.cardData.name}</div>
+              </div>
+            </div>
+            {productModal()}
+          </div>
         </Modal.Description>
       </Modal.Content>
     </Modal>
