b6b06af3d8314055d688ef91e25f8b2e2becfb8f
Mantas Ivaskevicius <mantasivaskevicius@gmail.com>
2020-02-12 19:26:28 -0500

    Finished working stars

--- src/components/RatingsAndReviews/Ratings/Ratings.jsx
+++ src/components/RatingsAndReviews/Ratings/Ratings.jsx
@@ -3,19 +3,21 @@ import RatingDiagram from "./RatingDiagram";
 
 var Ratings = ({ avg, metaData, changeStarFilter, currentStarFilter }) => {
   const starPercent = Math.round((avg / 5) * 100);
-  console.log(starPercent);
   return (
     <div className="ratingsParent">
       <div className="ratingsParentContainer">
-        <div className="rating">{Number(avg).toFixed(1)}</div>
-        <div className="star-ratings">
-          <div className="fill-ratings" style={{ width: `${starPercent}%` }}>
-            <p className="starSpan">★★★★★</p>
-          </div>
-          <div className="empty-ratings">
-            <p className="starSpan">★★★★★</p>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className="rating">{Number(avg).toFixed(1)}</div>
+          <div className="star-ratings">
+            <div className="fill-ratings" style={{ width: `${starPercent}%` }}>
+              <p className="starSpan">★★★★★</p>
+            </div>
+            <div className="empty-ratings">
+              <p className="starSpan">★★★★★</p>
+            </div>
           </div>
         </div>
+
         <div className="ratingDiagramParent">
           <RatingDiagram
             meta={metaData}
