df6cab798be89607c34c56ba801a52625dbcb604
Mantas Ivaskevicius <mantasivaskevicius@gmail.com>
2020-02-13 11:01:53 -0500

    Whatever

--- src/components/RatingsAndReviews/ReviewEntry.jsx
+++ src/components/RatingsAndReviews/ReviewEntry.jsx
@@ -2,53 +2,67 @@ import React from "react";
 import { Image } from "semantic-ui-react";
 import PictureModal from "./PictureModal";
 
-var ReviewEntry = ({ review, addHelpful, report }) => (
-  <div style={{ borderStyle: "nothing" }} className="reviewEntryParent">
-    <div className="ratingAndUser">
-      <div>✯{review.rating}</div>
-      <div>
-        {review.reviewer_name + " "}, {review.date.split("T")[0]}
-      </div>
-    </div>
+var ReviewEntry = ({ review, addHelpful, report }) => {
+  const starPercent = (review.rating / 5) * 100;
+  console.log("this is the rating", review.rating);
+  console.log(starPercent);
 
-    <div className="reviewSummary">{review.summary}</div>
-    <div className="reviewBody">{review.body}</div>
-    {review.photos.length > 0 ? (
-      <Image.Group size="small">
-        {review.photos.map(pic => (
-          <PictureModal url={pic.url} />
-        ))}
-      </Image.Group>
-    ) : null}
-    {review.recommend > 0 ? (
-      <div style={{ paddingBottom: "20px", fontStyle: "italic" }}>
-        {" "}
-        ✓ I recommend this product
-      </div>
-    ) : null}
-    {review.response !== null ? <div>{review.response}</div> : null}
-    <div className="helpfulParent">
-      <div className="helpfulWord">Helpful?</div>
-      <div className="yesButton" onClick={() => addHelpful(review.review_id)}>
-        Yes
-      </div>
-      <div className="helpfulCount">({review.helpfulness})</div>
-      <div style={{ paddingLeft: "10px" }}>|</div>
-      <div className="reportButton" onClick={() => report(review.review_id)}>
-        Report
-      </div>
-    </div>
+  return (
+    <div style={{ borderStyle: "nothing" }} className="reviewEntryParent">
+      <div className="ratingAndUser">
+        <div className="star-ratings" style={{ fontSize: "120%" }}>
+          <div className="fill-ratings" style={{ width: `${starPercent}%` }}>
+            <p className="starSpan">★★★★★</p>
+          </div>
+          <div className="empty-ratings">
+            <p className="starSpan">★★★★★</p>
+          </div>
+        </div>
 
-    <p
-      style={{
-        fontWeight: "lighter",
-        color: "darkgray",
-        maxWidth: "maxContent"
-      }}
-    >
-      ________________________________________________________________________________________________________________________________
-    </p>
-  </div>
-);
+        <div>
+          {review.reviewer_name + " "}, {review.date.split("T")[0]}
+        </div>
+      </div>
+
+      <div className="reviewSummary">{review.summary}</div>
+      <div className="reviewBody">{review.body}</div>
+      {review.photos.length > 0 ? (
+        <Image.Group size="small">
+          {review.photos.map(pic => (
+            <PictureModal url={pic.url} />
+          ))}
+        </Image.Group>
+      ) : null}
+      {review.recommend > 0 ? (
+        <div style={{ paddingBottom: "20px", fontStyle: "italic" }}>
+          {" "}
+          ✓ I recommend this product
+        </div>
+      ) : null}
+      {review.response !== null ? <div>{review.response}</div> : null}
+      <div className="helpfulParent">
+        <div className="helpfulWord">Helpful?</div>
+        <div className="yesButton" onClick={() => addHelpful(review.review_id)}>
+          Yes
+        </div>
+        <div className="helpfulCount">({review.helpfulness})</div>
+        <div style={{ paddingLeft: "10px" }}>|</div>
+        <div className="reportButton" onClick={() => report(review.review_id)}>
+          Report
+        </div>
+      </div>
+
+      <p
+        style={{
+          fontWeight: "lighter",
+          color: "darkgray",
+          maxWidth: "maxContent"
+        }}
+      >
+        ________________________________________________________________________________________________________________________________
+      </p>
+    </div>
+  );
+};
 
 export default ReviewEntry;
