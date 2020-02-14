9f7c0f35b0bd03e2ee8f7339b51177c77aa10102
Mantas Ivaskevicius <mantasivaskevicius@gmail.com>
2020-02-14 14:05:18 -0500

    Commit before presentation

--- src/components/RatingsAndReviews/ReviewForm.jsx
+++ src/components/RatingsAndReviews/ReviewForm.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import { Form, Button, Image, FormRadio } from "semantic-ui-react";
+import { Form, Button, Image, FormRadio, Rating } from "semantic-ui-react";
 import ReactFilestack from "filestack-react";
 import { apikey } from "./APIkey";
 import axios from "axios";
-import placeholder from "./placeholder.jpg";
 
 const basicOptions = {
   accept: "image/*",
@@ -25,6 +24,7 @@ class ReviewForm extends Component {
       recValue: "",
       comfortValue: "",
       qualityValue: "",
+      comfort: "",
       lengthValue: "",
       fitValue: "",
       header: "",
@@ -49,8 +49,8 @@ class ReviewForm extends Component {
 
   /*--------------- RADIO FORMS ---------------*/
 
-  handleChangeRadioRate = (e, { value }) => {
-    this.setState({ rateValue: value });
+  handleRate = (e, { rating }) => {
+    this.setState({ rateValue: rating });
   };
 
   handleChangeRadioRecommend = (e, { value }) => {
@@ -86,42 +86,74 @@ class ReviewForm extends Component {
     if (this.state.comfortValue !== "") validCount++;
     if (this.state.lengthValue !== "") validCount++;
     if (this.state.fitValue !== "") validCount++;
-    if (this.state.header !== "") validCount++;
     if (this.state.body.length >= 50) validCount++;
     if (this.state.name !== "") validCount++;
     if (this.state.email !== "") validCount++;
-    if (validCount === 10) {
+    if (validCount === 9) {
       this.handleSubmit();
     } else this.setState({ showValid: true });
   };
 
   handleSubmit = () => {
-    axios
-      .post(`http://3.134.102.30/reviews/${this.props.id}`, {
-        rating: Number(this.state.rateValue),
-        summary: this.state.header,
-        body: this.state.body,
-        recommend: this.state.recValue,
-        name: this.state.name,
-        email: this.state.email,
-        photos: this.state.images,
-        characteristics: {
-          [this.props.meta.characteristics.Fit.id]: Number(this.state.fitValue),
-          [this.props.meta.characteristics.Length.id]: Number(
-            this.state.lengthValue
-          ),
-          [this.props.meta.characteristics.Comfort.id]: Number(
-            this.state.comfortValue
-          ),
-          [this.props.meta.characteristics.Quality.id]: Number(
-            this.state.qualityValue
-          )
-        }
-      })
-      .then(response => {
-        this.props.closeModal(true);
-        this.setState({ showValid: false });
-      });
+    if (this.props.meta.characteristics.Size !== undefined) {
+      axios
+        .post(`http://3.134.102.30/reviews/${this.props.id}`, {
+          rating: Number(this.state.rateValue),
+          summary: this.state.header,
+          body: this.state.body,
+          recommend: this.state.recValue,
+          name: this.state.name,
+          email: this.state.email,
+          photos: this.state.images,
+          characteristics: {
+            [this.props.meta.characteristics.Size.id]: Number(
+              this.state.fitValue
+            ),
+            [this.props.meta.characteristics.Width.id]: Number(
+              this.state.lengthValue
+            ),
+            [this.props.meta.characteristics.Comfort.id]: Number(
+              this.state.comfortValue
+            ),
+            [this.props.meta.characteristics.Quality.id]: Number(
+              this.state.qualityValue
+            )
+          }
+        })
+        .then(response => {
+          this.props.closeModal(true);
+          this.setState({ showValid: false });
+        });
+    } else {
+      axios
+        .post(`http://3.134.102.30/reviews/${this.props.id}`, {
+          rating: Number(this.state.rateValue),
+          summary: this.state.header,
+          body: this.state.body,
+          recommend: this.state.recValue,
+          name: this.state.name,
+          email: this.state.email,
+          photos: this.state.images,
+          characteristics: {
+            [this.props.meta.characteristics.Fit.id]: Number(
+              this.state.fitValue
+            ),
+            [this.props.meta.characteristics.Length.id]: Number(
+              this.state.lengthValue
+            ),
+            [this.props.meta.characteristics.Comfort.id]: Number(
+              this.state.comfortValue
+            ),
+            [this.props.meta.characteristics.Quality.id]: Number(
+              this.state.qualityValue
+            )
+          }
+        })
+        .then(response => {
+          this.props.closeModal(true);
+          this.setState({ showValid: false });
+        });
+    }
   };
 
   /*--------------- Upload photo ---------------*/
@@ -141,35 +173,11 @@ class ReviewForm extends Component {
           <label>
             Rate it <span style={{ color: "red" }}> *</span>
           </label>
-          <FormRadio
-            label="1 star"
-            value="1"
-            checked={this.state.rateValue === "1"}
-            onChange={this.handleChangeRadioRate}
-          />
-          <FormRadio
-            label="2 star"
-            value="2"
-            checked={this.state.rateValue === "2"}
-            onChange={this.handleChangeRadioRate}
-          />
-          <FormRadio
-            label="3 star"
-            value="3"
-            checked={this.state.rateValue === "3"}
-            onChange={this.handleChangeRadioRate}
-          />
-          <FormRadio
-            label="4 star"
-            value="4"
-            checked={this.state.rateValue === "4"}
-            onChange={this.handleChangeRadioRate}
-          />
-          <FormRadio
-            label="5 star"
-            value="5"
-            checked={this.state.rateValue === "5"}
-            onChange={this.handleChangeRadioRate}
+          <Rating
+            maxRating={5}
+            onRate={this.handleRate}
+            size="massive"
+            icon="star"
           />
         </Form.Group>
         <br />
@@ -263,72 +271,144 @@ class ReviewForm extends Component {
               onChange={this.handleChangeRadioQuality}
             />
           </div>
-          <div>
-            <label className="characteristicsLabel">Length</label>
-            <FormRadio
-              label="Runs Short"
-              value="1"
-              checked={this.state.lengthValue === "1"}
-              onChange={this.handleChangeRadioLength}
-            />
-            <FormRadio
-              label="Runs slightly short"
-              value="2"
-              checked={this.state.lengthValue === "2"}
-              onChange={this.handleChangeRadioLength}
-            />
-            <FormRadio
-              label="Perfect"
-              value="3"
-              checked={this.state.lengthValue === "3"}
-              onChange={this.handleChangeRadioLength}
-            />
-            <FormRadio
-              label="Runs slightly long"
-              value="4"
-              checked={this.state.lengthValue === "4"}
-              onChange={this.handleChangeRadioLength}
-            />
-            <FormRadio
-              label="Runs long"
-              value="5"
-              checked={this.state.lengthValue === "5"}
-              onChange={this.handleChangeRadioLength}
-            />
-          </div>
-          <div>
-            <label className="characteristicsLabel">Fit</label>
-            <FormRadio
-              label="Runs tight"
-              value="1"
-              checked={this.state.fitValue === "1"}
-              onChange={this.handleChangeRadioFit}
-            />
-            <FormRadio
-              label="Runs slightly tight"
-              value="2"
-              checked={this.state.fitValue === "2"}
-              onChange={this.handleChangeRadioFit}
-            />
-            <FormRadio
-              label="Perfect"
-              value="3"
-              checked={this.state.fitValue === "3"}
-              onChange={this.handleChangeRadioFit}
-            />
-            <FormRadio
-              label="Runs slightly long"
-              value="4"
-              checked={this.state.fitValue === "4"}
-              onChange={this.handleChangeRadioFit}
-            />
-            <FormRadio
-              label="Runs long"
-              value="5"
-              checked={this.state.fitValue === "5"}
-              onChange={this.handleChangeRadioFit}
-            />
-          </div>
+          {this.props.meta.characteristics.Width !== undefined ? (
+            <div>
+              <label className="characteristicsLabel">Width</label>
+              <FormRadio
+                label="Too narrow"
+                value="1"
+                checked={this.state.lengthValue === "1"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Slightly narrow"
+                value="2"
+                checked={this.state.lengthValue === "2"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Perfect"
+                value="3"
+                checked={this.state.lengthValue === "3"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Slightly wide"
+                value="4"
+                checked={this.state.lengthValue === "4"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Too wide"
+                value="5"
+                checked={this.state.lengthValue === "5"}
+                onChange={this.handleChangeRadioLength}
+              />
+            </div>
+          ) : (
+            <div>
+              <label className="characteristicsLabel">Length</label>
+              <FormRadio
+                label="Runs Short"
+                value="1"
+                checked={this.state.lengthValue === "1"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Runs slightly short"
+                value="2"
+                checked={this.state.lengthValue === "2"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Perfect"
+                value="3"
+                checked={this.state.lengthValue === "3"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Runs slightly long"
+                value="4"
+                checked={this.state.lengthValue === "4"}
+                onChange={this.handleChangeRadioLength}
+              />
+              <FormRadio
+                label="Runs long"
+                value="5"
+                checked={this.state.lengthValue === "5"}
+                onChange={this.handleChangeRadioLength}
+              />
+            </div>
+          )}
+          {this.props.meta.characteristics.Size !== undefined ? (
+            <div>
+              <label className="characteristicsLabel">Size</label>
+              <FormRadio
+                label="A size too small"
+                value="1"
+                checked={this.state.fitValue === "1"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="½ a size too small"
+                value="2"
+                checked={this.state.fitValue === "2"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="Perfect"
+                value="3"
+                checked={this.state.fitValue === "3"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="½ a size too big"
+                value="4"
+                checked={this.state.fitValue === "4"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="A size too wide"
+                value="5"
+                checked={this.state.fitValue === "5"}
+                onChange={this.handleChangeRadioFit}
+              />
+            </div>
+          ) : (
+            <div>
+              <label className="characteristicsLabel">Fit</label>
+              <FormRadio
+                label="Runs tight"
+                value="1"
+                checked={this.state.fitValue === "1"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="Runs slightly tight"
+                value="2"
+                checked={this.state.fitValue === "2"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="Perfect"
+                value="3"
+                checked={this.state.fitValue === "3"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="Runs slightly long"
+                value="4"
+                checked={this.state.fitValue === "4"}
+                onChange={this.handleChangeRadioFit}
+              />
+              <FormRadio
+                label="Runs long"
+                value="5"
+                checked={this.state.fitValue === "5"}
+                onChange={this.handleChangeRadioFit}
+              />
+            </div>
+          )}
         </Form.Group>
 
         {this.state.showValid === true ? (
@@ -354,7 +434,6 @@ class ReviewForm extends Component {
             maxLength="60"
             placeholder="(Max: 60 characters)  Example: Best purchase ever!"
             onChange={this.handleChangeForm}
-            required
           />
         </Form.Group>
 
@@ -377,9 +456,6 @@ class ReviewForm extends Component {
           customRender={({ onPick }) => (
             <div>
               <strong>Upload pictures (Optional) </strong>
-              {/* <br />
-              <br /> */}
-              {/* style={{ display: "flexbox", flexDirection: "row" }} */}
               <div style={{ display: "inline" }}>
                 <Button color="green" compact onClick={onPick}>
                   Upload
